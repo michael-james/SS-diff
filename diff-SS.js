3cc4a35a573740dc83e26dcd12667c9c7c200b7d
Michael James <thelivingpc@gmail.com>
2018-12-01 14:55:37 -0500

    diff highlighting

--- diff-SS.js
+++ diff-SS.js
@@ -20,47 +20,50 @@ function escape(s) {
 }
 
 function diffString( o, n ) {
-  o = o.replace(/\s+$/, '');
-  n = n.replace(/\s+$/, '');
+  // o = o.replace(/\s+$/, '');
+  // n = n.replace(/\s+$/, '');
 
-  var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) ); // split by whitespace
+  // var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) ); // split by whitespace
+  var out = diff(o == "" ? [] : o.split(''), n == "" ? [] : n.split('') ); // split every charac
+
+  // console.log(out);
 
   var str = "";
 
-  var oSpace = o.match(/\s+/g);
-  if (oSpace == null) {
-    oSpace = ["\n"];
-  } else {
-    oSpace.push("\n");
-  }
-  var nSpace = n.match(/\s+/g);
-  if (nSpace == null) {
-    nSpace = ["\n"];
-  } else {
-    nSpace.push("\n");
-  }
+  // var oSpace = o.match(/\s+/g);
+  // if (oSpace == null) {
+  //   oSpace = ["\n"];
+  // } else {
+  //   oSpace.push("\n");
+  // }
+  // var nSpace = n.match(/\s+/g);
+  // if (nSpace == null) {
+  //   nSpace = ["\n"];
+  // } else {
+  //   nSpace.push("\n");
+  // }
 
   if (out.n.length == 0) {
       for (var i = 0; i < out.o.length; i++) {
-        str += '<del>' + escape(out.o[i]) + oSpace[i] + "</del>";
+        str += '<del>' + escape(out.o[i]) + "</del>";
       }
   } else {
     if (out.n[0].text == null) {
       for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
-        str += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
+        str += '<del>' + escape(out.o[n]) + "</del>";
       }
     }
 
     for ( var i = 0; i < out.n.length; i++ ) {
       if (out.n[i].text == null) {
-        str += '<ins>' + escape(out.n[i]) + nSpace[i] + "</ins>";
+        str += '<ins>' + escape(out.n[i]) + "</ins>";
       } else {
         var pre = "";
 
         for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++ ) {
-          pre += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
+          pre += '<del>' + escape(out.o[n]) + "</del>";
         }
-        str += "" + out.n[i].text + nSpace[i] + pre;
+        str += "" + "<mute>" + out.n[i].text + "</mute>" + pre;
       }
     }
   }
